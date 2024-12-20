3bd7df5e1dc433e25b137f9a0d89e8b30fed48ac
Perumal Natarajan <perumal.ns87@hotmail.com>
2024-12-20 22:36:21 +0530

    again change the python code by updating the IP

--- myfirstapp.py
+++ myfirstapp.py
@@ -9,4 +9,4 @@ def home():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=443)
+    app.run(host="20.192.170.13", port=443)
