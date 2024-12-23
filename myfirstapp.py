2c046d974202b319fb983fb101aa4790b8f3a877
Perumal Natarajan <perumal.ns87@hotmail.com>
2024-12-23 14:13:52 +0530

    again changed the code, I guess this is the 5th time

--- myfirstapp.py
+++ myfirstapp.py
@@ -8,5 +8,5 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 443))
-    app.run(host="0.0.0.0", port='PORT')
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
